e5c7640e6cb267b4f374547c2cce61d96e199657
Your Name <you@example.com>
2020-11-26 18:50:34 +0300

    database

--- src/App.js
+++ src/App.js
@@ -1,101 +1,11 @@
 import Header from "./Header";
 import Content from "./Content";
-import Button from "./Button";
-import { useState } from "react";
-function App(props) {
-  const [database, setdatabase] = useState([
-    {
-      id: 1,
-      name: "MacBook",
-      price: 100000,
-      bought: false,
-      rating: 5,
-      image: "http://intocode.ru/d/react-project-1/images/1.jpg"
-    },
-    {
-      id: 2,
-      name: "Galaxy",
-      price: 35999,
-      bought: false,
-      rating: 4,
-      image: "http://intocode.ru/d/react-project-1/images/2.jpg"
-    },
-    {
-      id: 3,
-      name: "Скутер",
-      price: 65500,
-      bought: false,
-      rating: 5,
-      image: "http://intocode.ru/d/react-project-1/images/3.jpg"
-    },
-    {
-      id: 4,
-      name: "Монитор Samsung",
-      price: 12000,
-      bought: false,
-      rating: 3,
-      image: "http://intocode.ru/d/react-project-1/images/4.jpg"
-    },
-    {
-      id: 5,
-      name: "Респераторная маска",
-      price: 500,
-      bought: false,
-      rating: 5,
-      image: "http://intocode.ru/d/react-project-1/images/5.jpg"
-    },
-    {
-      id: 6,
-      name: "Стиральная машина",
-      price: 100000,
-      bought: false,
-      rating: 3,
-      image: "http://intocode.ru/d/react-project-1/images/6.jpg"
-    },
-    {
-      id: 7,
-      name: "Белый холодильник",
-      price: 43100,
-      bought: false,
-      rating: 5,
-      image: "http://intocode.ru/d/react-project-1/images/7.jpg"
-    },
-    {
-      id: 8,
-      name: "Колонка",
-      price: 3000,
-      bought: false,
-      rating: 4,
-      image: "http://intocode.ru/d/react-project-1/images/8.jpg"
-    },
-    {
-      id: 9,
-      name: "Наушники",
-      price: 1500,
-      bought: false,
-      rating: 5,
-      image: "http://intocode.ru/d/react-project-1/images/9.jpg"
-    }
-  ]);
-  const product=database.map((plan)=>{
-    return(
-      <div className="card">  
-<div className="card-image">
-  <img src={plan.image} alt="ac" />
-</div>
-<div className="card-info">
-  <div className="card-name">{plan.name}</div>
-  <div className="card-price">{plan.price} ₽</div>
-  <Button/>
-</div> 
-</div>
-    )
-  });
- setdatabase(product)
+function App() {
+  
   return (
     <div className="app">
-        <Header/>
-        <Content product={product}/>
+        <Header />
+        <Content/>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,17 @@
+import Button from "./Button";
 function Card(props){
+    
     return(
-        <>
-        {props.product}
-        </>
+        <div className="card">
+            <div className="card-image">
+                <img src={props.product.image} alt="" />
+            </div>
+            <div className="card-info">
+                <div className="card-name">{props.product.name}</div>
+                <div className="card-price">{props.product.price}₽</div>
+                <Button/>
+            </div>
+        </div>
     )
 }
 export default Card;
